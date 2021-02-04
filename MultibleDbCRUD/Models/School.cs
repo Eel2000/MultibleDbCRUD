4e8a6569eead2abb3c33111440f4f9be86244731
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2021-02-04 11:05:02 +0100

    Add Student And School Models

---
+++ MultibleDbCRUD/Models/School.cs
@@ -0,0 +1,10 @@
+﻿namespace MultibleDbCRUD.Models
+{
+    public class School
+    {
+        public string ID_school { get; set; }
+        public string SchoolName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
