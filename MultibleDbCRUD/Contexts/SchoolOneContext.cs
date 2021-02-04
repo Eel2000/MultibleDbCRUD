003c3a55586a574ecdbf8f13d08786e1aa976d00
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2021-02-04 11:10:34 +0100

    🚧Add First Context Of App.

---
+++ MultibleDbCRUD/Contexts/SchoolOneContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultibleDbCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultibleDbCRUD.Contexts
+{
+    public class SchoolOneContext : DbContext
+    {
+        public SchoolOneContext()
+        {
+
+        }
+
+        public SchoolOneContext(DbContextOptions<SchoolOneContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<School> Schools { get; set; }
+    }
+}
