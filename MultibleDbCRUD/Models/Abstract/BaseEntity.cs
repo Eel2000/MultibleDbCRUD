3298fb0d44f01afea1c5be900e27de4294700a0c
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2021-02-04 11:47:41 +0100

    ✨Add Base Repository Interfaces

---
+++ MultibleDbCRUD/Models/Abstract/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultibleDbCRUD.Models.Abstract
+{
+    public abstract class BaseEntity
+    {
+    }
+}
---
+++ MultibleDbCRUD/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultibleDbCRUD.Models.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultibleDbCRUD.Repositories.Interfaces
+{
+    public interface IBaseRepository<TContext, T> where TContext : DbContext where T : BaseEntity
+    {
+        List<T> GetAll();
+        T Find(string id);
+
+        void Add(T value);
+
+        void SaveChange();
+    }
+}
