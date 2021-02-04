8abb1e3865749a9f88dd750e6499428993607ac9
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2021-02-04 11:13:30 +0100

    🚧Add School Two Context

---
+++ MultibleDbCRUD/Contexts/SchoolTwoContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultibleDbCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultibleDbCRUD.Contexts
+{
+    public class SchoolTwoContext :DbContext
+    {
+        public SchoolTwoContext()
+        {
+
+        }
+
+        public SchoolTwoContext(DbContextOptions<SchoolTwoContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<School> Schools { get; set; }
+    }
+}
