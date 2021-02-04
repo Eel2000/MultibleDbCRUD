b915587ff925d4605a5b6b5e9730f29f6c59692f
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2021-02-04 12:18:47 +0100

    🚧Update Interface Name

---
+++ MultibleDbCRUD/Services/Interfaces/IMainService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultibleDbCRUD.Services.Interfaces
+{
+    public interface IMainService
+    {
+        Task<IEnumerable<T>> GetAllSchool<T>();
+        Task<IEnumerable<T>> GetAllStudentFromSchool<T>();
+        Task Add(string firstname, string lastname, string school);
+    }
+}
